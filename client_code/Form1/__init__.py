fc5ec5ef8883b8c3648849dff25cf38865eac02b
25892@aevf.pt <25892@aevf.pt>
2024-11-25 17:12:58 +0000

    Created giphy1-ezgif.com-gif-to-webp-converter.webp; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,3 +8,12 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    print(self.text_box_1.text)
+    pass
+
+  def outlined_button_1_show(self, **event_args):
+    """This method is called when the Button is shown on the screen"""
+    pass
