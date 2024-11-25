0e815bacc43772b324789971e290409e3558f496
25892@aevf.pt <25892@aevf.pt>
2024-11-25 19:47:32 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-
+from PopupForm import PopupForm
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -12,6 +12,7 @@ class Form1(Form1Template):
   def outlined_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     print(self.text_box_1.text)
+    resul = alert(PopUpForm)
     pass
 
   def outlined_button_1_show(self, **event_args):
